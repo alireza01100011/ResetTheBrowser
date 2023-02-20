475f377284b6481c7b2e707f88dddffa264b54dc
Alireza <armanparsi206@outlook.com>
2023-02-20 22:06:38 +0330

    Delet Data Opera

---
+++ Opera.bat
@@ -0,0 +1,2 @@
+cd "C:\Users\%username%\AppData\Roaming\" && C: && rmdir /s /q "Opera Software\Opera Stable"
+cd "C:\Users\%username%\AppData\Local\" && C: && rmdir /s /q "Opera Software\Opera Stable"
